5a59b0c66e9d75978171ed6895fe0eb8f9f97c80
Chiemena Godswill Onyeakazi <chiemenagodswill97@gmail.com>
2026-09-21 15:28:30 +0100

    day1 code

---
+++ day1/index.php
@@ -0,0 +1,60 @@
+<?php
+
+    error_reporting(E_ALL);
+  ini_set('display_errors', 1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>day1</title>
+</head>
+<body>
+    
+    <?php 
+    
+        $tech = "php";
+        $name = "IONS";
+
+        // echo 'my name is $name' . "<br>";
+
+        $colors = ["Red", "Green", "Blue"];
+        $colors[] = "Yellow"; // Append an element
+        
+        // 2. Associative Array
+        $user = [
+            "username" => "admin_alex",
+            "role" => "Admin"
+        ];
+        
+        // 3. Essential Array Utilities
+        $total = count($colors); // Returns: 4
+        $is_red_present = in_array("Red", $colors); // Returns: true
+
+        // $input = $_POST['username'] ?? 'john'; // Null coalescing operator fallback
+
+        if (isset($input) && !empty($input)) {
+            echo "Username is provided." . "<br>";
+        }else{
+            echo "Username is not provided" . "<br>";
+        }
+        
+        // Inspecting structures during debugging:
+        // var_dump($colors); 
+
+        function calculateTotal($price, $taxRate = 0.05) {
+            $total = $price + ($price * $taxRate);
+            return $total;
+        }
+        
+        $bill = calculateTotal(100); // Returns 105
+        echo "<p>Your total bill is $$bill</p>";
+    
+    ?>
+    <h1>today we are learning <?php echo $tech ?></h1>
+    <h1>today we are learning <?= $tech ?></h1>
+</body>
+</html>
